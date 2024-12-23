1c83a27b4082af8601d1595d989f31c43d3973f4
Muzammil Usman <uraza2520@gmail.com>
2024-12-24 02:19:48 +0500

    75% Done

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 function through(e) {
   e.preventDefault();
-  window.location.replace("quiz.html");
+  if (redFlag == true) {
+    window.location.replace("quiz.html");
+  }
 }
+
+var inputLength = document.getElementById("username");
+
+function nameLengthChecker(e) {
+  if (e.target.value.length < 3) {
+    nameError.innerText = "Name should be greater than 2 character";
+    nameError.style.display = "block";
+    return;
+  }
+  if (!regex.test(e.target.value)) {
+    nameError.innerText = "Numbers not allowed";
+    nameError.style.display = "block";
+    return;
+  }
+  nameError.style.display = "none";
+  redFlag = true;
+  var userData = {
+    username: e.target.value,
+  };
+  localStorage.setItem("userName", JSON.stringify(userData));
+}
+
+var nameError = document.getElementById("error");
+var redFlag = false;
+var regex = /^[a-zA-Z\s]*$/;
